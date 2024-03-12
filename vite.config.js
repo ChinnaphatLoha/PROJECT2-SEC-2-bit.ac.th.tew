30ed6b6da9ea3c5fa90f8fc22b8bf2eff51df277
Kittipat Bualoylom <110712358+Tonnamkit@users.noreply.github.com>
2024-03-12 14:28:13 +0700

    Update : uncomment base

--- vite.config.js
+++ vite.config.js
@@ -13,5 +13,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  // base: 'PROJECT2-SEC-2-bit.ac.th.tew'
+  base: 'PROJECT2-SEC-2-bit.ac.th.tew'
 })
