c18b2b9d63a31db9e63a36c2062a5745dd60a152
ChinnaphatLoha <chinnaphat.loha@mail.kmutt.ac.th>
2024-03-14 03:33:11 +0700

    feat: Initialize Meeting service and introduce new endpoint for managing meetings

--- src/api/provider.js
+++ src/api/provider.js
@@ -2,7 +2,13 @@ import app from '../backend/app.js'
 
 class Provider {
   static async request(uri, init = null) {
-    return await app.call(uri, init)
+    try {
+      const response = await app.call(uri, init)
+      return response
+    } catch (error) {
+      console.error('Request failed:', error)
+      return new Response(null, { status: 500, statusText: 'Internal Server Error' })
+    }
   }
 }
 
---
+++ src/backend/app/dto/meeting-dto.js
@@ -0,0 +1,32 @@
+import { getAllUsers } from '../schema'
+
+const removeGroupPropertyFromFeedback = (feedback) => {
+  const feedbackRecords = {}
+  for (const key in feedback) {
+    // eslint-disable-next-line no-unused-vars
+    feedbackRecords[key] = feedback[key].map(({ group, ...rest }) => rest)
+  }
+  return feedbackRecords
+}
+
+export const getMeetingsDTO = async (meetings, feedbacks) => {
+  const ALL_USERS = await getAllUsers()
+  const [meetingsDTO] = meetings.map((meeting) => {
+    const uncleanedFeedbackRecords = Object.groupBy(
+      feedbacks.map((feedback) => ({
+        content: feedback.text,
+        group: feedback.group,
+        username: ALL_USERS.find((user) => user.id === feedback.userId).username
+      })),
+      ({ group }) => group
+    )
+    return {
+      id: meeting.id,
+      start_date: meeting.start_date,
+      end_date: meeting.end_date,
+      description: meeting.description,
+      feedbackRecords: removeGroupPropertyFromFeedback(uncleanedFeedbackRecords)
+    }
+  })
+  return meetingsDTO
+}
