3ec62fbfd26e73717b7142db3696670b2bafb4db
Tew2945 <teeratorn.thie@mail.kmutt.ac.th>
2024-03-17 17:26:38 +0700

    feat: Add search bar into nevbar

--- src/common/components/LayoutComponent.vue
+++ src/common/components/LayoutComponent.vue
@@ -9,7 +9,22 @@ import { RouterLink } from 'vue-router'
         <RouterLink to="" class="link text-2xl">Project</RouterLink>
         <RouterLink to="" class="link text-2xl">Retro</RouterLink>
       </div>
-      <div class="flex items-center justify-between w-[18%]">
+      <div class="flex items-center justify-between w-[40%]">
+        <label class="input input-bordered flex items-center gap-2">
+          <input type="text" class="grow" placeholder="Search" />
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 16 16"
+            fill="#000000"
+            class="w-4 h-4 opacity-70"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
+              clip-rule="evenodd"
+            />
+          </svg>
+        </label>
         <RouterLink to="" class="link text-xl">Hello, TEW</RouterLink>
         <RouterLink to="" class="btn btn-sign-out">Sign-out</RouterLink>
       </div>
