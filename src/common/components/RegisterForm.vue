24e61850e8372cd5d7b28a248528be2725f7a5b8
BomScoob <sorawitkraukham@gmail.com>
2024-03-20 20:50:54 +0700

    feat : Register is done

--- src/common/components/RegisterForm.vue
+++ src/common/components/RegisterForm.vue
@@ -3,58 +3,25 @@ import TextForm from './TextForm.vue'
 import SubmitBtn from './SubmitBtn.vue'
 import getFormUtils from '../utils/form-utils'
 import { USER_ATTRIBUTE } from '../constants/user-attributes'
-import Provider from '@/api/provider'
-import { ACCOUNT_ENDPOINTS } from '../constants/uri-endpoints'
-import { ref, onMounted } from 'vue'
-import uuid from '../utils/uuid'
+import { useUserStore } from '@/stores/store'
 
 const registerFormUtils = getFormUtils()
-const isAvailableUsername = ref(true)
-const newUser = {
-  username: '',
-  password: ''
-}
-
-onMounted(()=> {
-  console.log('Mounted Register')
-})
-
-const checkUniqueUsername = async () => {
-  const { username } = registerFormUtils.getObject()
-  newUser.username = username
-  const res = await Provider.request(ACCOUNT_ENDPOINTS.availability + `?username=${username}`)
-  const data = await res.json()
-  isAvailableUsername.value = data.available
-  if (isAvailableUsername.value) {
-    generatePassword()
-    registerNewUser()
-  } else {
-    alert('Duplicate username.')
-  }
-}
-
-const generatePassword = () => {
-  const generatedPassword = uuid()
-  newUser.password = generatedPassword
-}
+const userStore = useUserStore()
 
 const registerNewUser = async () => {
-  const res = await Provider.request(ACCOUNT_ENDPOINTS.register, {
-    body: JSON.stringify(newUser)
-  })
-  const data = res.ok ? res.json() : null
-  if (data === null) {
-    console.error('Data is null ' + data)
-  }
-  //? keep data to pinia store
-  console.log('store data to pinia state manager')
+  const { username } = registerFormUtils.getObject()
+  userStore.registerNewUser(username, testingError)
+}
+
+const testingError = (msg) => {
+  console.log(msg)
 }
 </script>
 
 <template>
   <div>
     <h1>Register</h1>
-    <form @submit.prevent="checkUniqueUsername">
+    <form @submit.prevent="registerNewUser">
       <TextForm
         @update:textValue="registerFormUtils.setTextValue(USER_ATTRIBUTE.username, $event)"
         placeholderText="Enter your username"
