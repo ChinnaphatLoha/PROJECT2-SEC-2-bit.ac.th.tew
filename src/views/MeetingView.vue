dd4d6d7c1495f91adff37d54b33a92533d7625ef
ChinnaphatLoha <chinnaphat.loha@mail.kmutt.ac.th>
2024-03-21 22:00:58 +0700

    fix: Ensure rendering and leverage reactive data

--- src/views/MeetingView.vue
+++ src/views/MeetingView.vue
@@ -1,30 +1,24 @@
 <script setup>
-// import { ref } from 'vue'
+import { ref } from 'vue'
 import CollapseBar from '@/common/components/CollapseBar.vue'
 import ListWrap from '@/common/components/ListWrap.vue'
 import CardRetro from '@/common/components/CardRetro.vue'
 import PlusIcon from '@/common/components/icons/PlusIcon.vue'
 import Provider from '@/api/provider'
-// import { computed, ref } from 'vue'
-const items = [
-  { id: '1', description: 'Hello world'},
-  { id: '2', description: 'Hello world'},
-  { id: '3', description: 'Hello world'},
-  { id: '4', description: 'Hello world'},
-  { id: '5', description: 'Hello world'},
-  { id: '6', description: 'Hello world'},
-  { id: '7', description: 'Hello world'},
-  { id: '8', description: 'Hello world'}
-]
-const response = await Provider.request('/api/project-composition/meetings?pid=c03a')
-const data = response.ok ? await response.json() : null
-console.log(data)
+
+const loadProjectMeetings = async (items) => {
+  const response = await Provider.request('/api/project-composition/meetings?pid=c03a')
+  const data = response.ok ? await response.json() : null
+  items.value = data
+}
+const items = ref([])
+loadProjectMeetings(items)
 </script>
 
 <template>
   <div class="m-8 card-retro border-dashed flex flex-col gap-4 items-center justify-center">
-      <PlusIcon :size="'w-12'" />
-      <p class="create-title">Create Meeting</p>
+    <PlusIcon :size="'w-12'" />
+    <p class="create-title">Create Meeting</p>
   </div>
   <hr />
   <div class="m-4 mt-8">
