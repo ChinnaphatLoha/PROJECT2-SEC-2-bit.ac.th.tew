d72479d93de739b3269191f1a21ab0218f858dcc
BomScoob <sorawitkraukham@gmail.com>
2024-03-28 22:12:43 +0700

    fix: now we can use feedback by polling update

--- src/views/RetroFeedBackView.vue
+++ src/views/RetroFeedBackView.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref, reactive, watch, computed } from 'vue'
+import { ref, reactive, watch, computed, onMounted, onUnmounted } from 'vue'
 import { useRoute, useRouter } from 'vue-router'
 import RetroColumn from '@/common/components/retro_feedback/RetroColumn.vue'
 import DeleteDialog from '@/common/components/DeleteDialog.vue'
@@ -37,21 +37,91 @@ const goToMeetingEdit = () => {
 }
 
 const meeting = reactive({ items: useStore.meeting })
+const isMeetingEnd = ref(false)
+const endMeetingTime = new Date(meeting.items.end_date).getTime()
 const start_datetime = computed(() =>
   formatDateTime(new Date(meeting.items.start_date), '[date, time]')
 )
 const end_datetime = computed(() =>
   formatDateTime(new Date(meeting.items.end_date), '[date, time]')
 )
-watch(
-  () => useStore.$state.ownedProjects,
-  (newValue) => {
-    meeting.items = newValue
-      .flatMap((project) => project.meetings)
-      .flat()
-      .find((m) => m.id === meetingId)
-    console.log(meeting.items)
+
+const displayMeetingData = computed(() => {
+  return meeting
+})
+
+const checkMeetingEndAndEditStatus = async (end_datetime) => {
+  const currentDateTime = new Date().getTime()
+  console.log(currentDateTime)
+  console.log(end_datetime)
+  if (currentDateTime > end_datetime && op) {
+    isMeetingEnd.value = true
+    console.log('Meeting is end')
+  } else {
+    isMeetingEnd.value = false
+    console.log('Meeting is not end')
   }
+}
+
+const pollingData = (fetchDataCallback, intervalTime = 5000, endMeetingTime) => {
+  let i = 0
+  checkMeetingEndAndEditStatus(endMeetingTime)
+  let polling = setInterval(async () => {
+    if (isMeetingEnd.value) {
+      // clear interval loop
+      await new Promise((resolve) => {
+        clearInterval(polling)
+        resolve()
+      })
+    } else {
+      console.log(i)
+      await fetchDataCallback()
+    }
+    i++
+  }, intervalTime)
+}
+
+const fetchMeetingData = async () => {
+  await useStore.getFeedbacksByMeetingId(meetingId, showErrorMsg)
+  meeting.items = useStore.meeting
+  console.log('------- PATCH --------')
+}
+
+const showErrorMsg = (msg) => {
+  console.log(msg)
+}
+
+onMounted(() => {
+  console.log('START POLLING')
+  if (!isMeetingEnd.value) {
+    pollingData(fetchMeetingData, 5000, endMeetingTime)
+  } else {
+    console.log('Meeting is end. STOP POLLING')
+  }
+})
+
+onUnmounted(() => {
+  console.log('STOP POLLING')
+  isMeetingEnd.value = true
+})
+
+// watch(
+//   () => useStore.$state.ownedProjects,
+//   (newValue) => {
+//     meeting.items = newValue
+//       .flatMap((project) => project.meetings)
+//       .flat()
+//       .find((m) => m.id === meetingId)
+//     console.log(meeting.items)
+//   }
+// )
+watch(
+  meeting,
+  (newValue) => {
+    console.log('Meeting data changed')
+    console.log(newValue)
+  },
+  { deep: true }
 )
 </script>
 
@@ -138,8 +208,8 @@ watch(
       <p class="text-lg ml-4">{{ meeting.items.description }}</p>
       <div class="flex gap-8 justify-center items-start my-16">
         <RetroColumn
-          v-for="{ feedbackRecords, end_date, id } in meeting"
-          :key="id"
+          v-for="{ feedbackRecords, end_date, index } in displayMeetingData"
+          :key="new Date().getTime() + index"
           :endDate="end_date"
           :feedbackRecords="feedbackRecords"
         />
