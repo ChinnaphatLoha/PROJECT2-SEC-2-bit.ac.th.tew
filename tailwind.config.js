71723269130d2c8ca271343240579d7a45224297
FameNu <110712106+FameNu@users.noreply.github.com>
2024-03-28 16:50:05 +0700

    add new theme color and modify bg-color

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 export default {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        'tan-hide': {
+          50: '#fef6ee',
+          100: '#feebd6',
+          200: '#fbd3ad',
+          300: '#f8b379',
+          400: '#f6995c',
+          500: '#f1691e',
+          600: '#e34f13',
+          700: '#bc3b12',
+          800: '#952f17',
+          900: '#782a16',
+          950: '#411209'
+        }
+      }
+    }
   },
   plugins: [require('daisyui')]
 }
