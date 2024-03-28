0e2c423f6e83541cd9b6875a0f8bab7108429404
ChinnaphatLoha <chinnaphat.loha@mail.kmutt.ac.th>
2024-03-28 16:19:41 +0700

    fix: Add token check for cookie expiration

--- src/common/config/globalSetup.js
+++ src/common/config/globalSetup.js
@@ -1,13 +1,19 @@
 import { ref, onMounted } from 'vue'
 import { TOKEN_KEY } from '@/backend/app/constants/token'
 import { setCookie } from '@/backend/utils/cookie-session-utils'
+import { getCookie } from '../utils/cookie-util'
 
 const setup = () => {
   const isCookieExpired = ref(false)
 
   const checkCookieExpiration = () => {
     const cookieExpiration = localStorage.getItem('cookieExpiration')
+    const token = getCookie(TOKEN_KEY)
     if (cookieExpiration) {
+      if (!token) {
+        isCookieExpired.value = true
+        return
+      }
       const expirationDate = new Date(cookieExpiration)
       const currentDate = new Date()
       isCookieExpired.value = expirationDate < currentDate
